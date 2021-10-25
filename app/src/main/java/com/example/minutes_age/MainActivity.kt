57fba6be9550556d90c28997751d0bc088746847
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-10-25 21:11:36 +0300

    progress update

--- app/src/main/java/com/example/minutes_age/MainActivity.kt
+++ app/src/main/java/com/example/minutes_age/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.minutes_age
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
@@ -23,32 +22,22 @@ class MainActivity : AppCompatActivity() {
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
         val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDay ->
-            Toast.makeText(this, "Chosen month is " + selectedMonth, Toast.LENGTH_SHORT).show()
 
-            //findViewById(R.id.tvSelectedDate).text : String = "hello" //"$selectedDay/${selectedMonth+1}/$selectedYear"
             val selectedDate = "$selectedDay/${selectedMonth+1}/$selectedYear"
-            //val currentDate = "$day/${month+1}/$year"
-            //val currentDate = Date()
+
             val textViewSelectedDate : TextView = findViewById(R.id.tvSelectedDate)
             textViewSelectedDate.text = selectedDate
 
             val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-            val theDate = sdf.parse(selectedDate)
-            val selectedDateInMinutes = theDate!!.time / 60000
+
+            val selectedDateInMinutes = (sdf.parse(selectedDate))!!.time / 60000//theDate!!.time / 60000
             val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
             val currentDateInMinutes = currentDate!!.time / 60000
             val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
 
-            Toast.makeText(this, "Difference is " + differenceInMinutes, Toast.LENGTH_LONG).show()
-
             val textViewDifferenceInMinutes : TextView = findViewById(R.id.tvSelectedDateInMinutes)
             textViewDifferenceInMinutes.text = differenceInMinutes.toString()
-           // textViewSelectedDate.text = selectedDate
-            //val oldDate = sdf.parse(currentDate)
-            //val diffDate =
-
-            //val sdfNew = SimpleDateFormat (day, month, year)
-            //val currentDate = sdfNew.parse(day, month, year)
 
         }, year, month, day)
 
